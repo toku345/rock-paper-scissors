dce2a2bf035ef8a1a0392bf4d750ee6587830eb6
Fumitaka Tokumitsu <toku345@gmail.com>
2018-08-22 09:39:31 +0900

    参照元のロダクションコード削除

--- src/main/demo/subs.cljs
+++
@@ -1,7 +0,0 @@
-(ns demo.subs
-  (:require [re-frame.core :as re-frame]))
-
-(re-frame/reg-sub
- ::app-view
- (fn [{:keys [page]}]
-   {:page-id page}))
--- src/test/demo/core_test.cljs
+++
@@ -1,5 +0,0 @@
-(ns demo.core-test
-  (:require [cljs.test :refer [deftest is testing]]))
-
-(deftest example-test
-  (is (= true true)))
